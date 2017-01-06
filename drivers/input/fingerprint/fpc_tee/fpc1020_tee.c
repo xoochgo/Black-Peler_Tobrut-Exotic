900709dc40f0a6beb88764bdafb9abb6fbbe445b
Bruno Martins <bgcngm@gmail.com>
2017-01-06 19:21:01 +0000

    input: fpc1020: Allow filtering of key events
     * Implement a key handler with the simple purpose of filtering
       the key events sent by the fingerprint HAL.
    
    [wight554: enforce filtering of all key events]
    
    Change-Id: I114493e39b61df9307fea584dd68391698f320f3
    Signed-off-by: Volodymyr Zhdanov <wight554@gmail.com>
    
    [@neobuddy89: adapt for sweet]
    
    Signed-off-by: Salllz <sal235222727@gmail.com>
    Change-Id: Ic710352c47b06ff0596423d0aeff27d7e50e2bf7
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>

--- drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
+++ drivers/input/fingerprint/fpc_tee/fpc1020_tee.c
@@ -32,6 +32,7 @@
 #include <linux/atomic.h>
 #include <linux/delay.h>
 #include <linux/gpio.h>
+#include <linux/input.h>
 #include <linux/interrupt.h>
 #include <linux/kernel.h>
 #include <linux/module.h>
@@ -44,6 +45,8 @@
 #include <linux/fb.h>
 #include <drm/drm_bridge.h>
 #include <drm/drm_notifier.h>
+
+#define FPC1020_NAME "fpc1020"
 
 #define FPC_GPIO_NO_DEFAULT -1
 #define FPC_TTW_HOLD_TIME 2000
@@ -119,6 +122,9 @@ struct fpc1020_data {
 	bool fb_black;
 	bool wait_finger_down;
 	struct work_struct work;
+#endif
+
+	struct input_handler input_handler;
 };
 
 
@@ -779,6 +785,63 @@ static const struct attribute_group attribute_group = {
 	.attrs = attributes,
 };
 
+static int input_connect(struct input_handler *handler,
+		struct input_dev *dev, const struct input_device_id *id) {
+	int rc;
+	struct input_handle *handle;
+	struct fpc1020_data *fpc1020 =
+		container_of(handler, struct fpc1020_data, input_handler);
+
+	if (!strstr(dev->name, "uinput-fpc"))
+		return -ENODEV;
+
+	handle = kzalloc(sizeof(struct input_handle), GFP_KERNEL);
+	if (!handle)
+		return -ENOMEM;
+
+	handle->dev = dev;
+	handle->handler = handler;
+	handle->name = FPC1020_NAME;
+	handle->private = fpc1020;
+
+	rc = input_register_handle(handle);
+	if (rc)
+		goto err_input_register_handle;
+
+	rc = input_open_device(handle);
+	if (rc)
+		goto err_input_open_device;
+
+	return 0;
+
+err_input_open_device:
+	input_unregister_handle(handle);
+err_input_register_handle:
+	kfree(handle);
+	return rc;
+}
+
+static bool input_filter(struct input_handle *handle, unsigned int type,
+		unsigned int code, int value)
+{
+	return true;
+}
+
+static void input_disconnect(struct input_handle *handle)
+{
+	input_close_device(handle);
+	input_unregister_handle(handle);
+	kfree(handle);
+}
+
+static const struct input_device_id ids[] = {
+	{
+		.flags = INPUT_DEVICE_ID_MATCH_EVBIT,
+		.evbit = { BIT_MASK(EV_KEY) },
+	},
+	{ },
+};
+
 #ifndef FPC_DRM_INTERFACE_WA
 static void notification_work(struct work_struct *work)
 {
@@ -948,6 +1011,17 @@ static int fpc1020_probe(struct platform_device *pdev)
 
 	wakeup_source_init(&fpc1020->ttw_wl, "fpc_ttw_wl");
 
+	fpc1020->input_handler.filter = input_filter;
+	fpc1020->input_handler.connect = input_connect;
+	fpc1020->input_handler.disconnect = input_disconnect;
+	fpc1020->input_handler.name = FPC1020_NAME;
+	fpc1020->input_handler.id_table = ids;
+	rc = input_register_handler(&fpc1020->input_handler);
+	if (rc) {
+		dev_err(dev, "failed to register key handler\n");
+		goto exit;
+	}
+
 	rc = sysfs_create_group(&dev->kobj, &attribute_group);
 	if (rc) {
 		dev_err(dev, "fpc could not create sysfs\n");
@@ -1010,12 +1084,12 @@ MODULE_DEVICE_TABLE(of, fpc1020_of_match);
 
 static struct platform_driver fpc1020_driver = {
 	.driver = {
-		.name	= "fpc1020",
-		.owner	= THIS_MODULE,
-		.of_match_table = fpc1020_of_match,
-	},
-	.probe	= fpc1020_probe,
-	.remove	= fpc1020_remove,
+		   .name = FPC1020_NAME,
+		   .owner = THIS_MODULE,
+		   .of_match_table = fpc1020_of_match,
+		   },
+	.probe = fpc1020_probe,
+	.remove = fpc1020_remove,
 };
 
 static int __init fpc1020_init(void)
