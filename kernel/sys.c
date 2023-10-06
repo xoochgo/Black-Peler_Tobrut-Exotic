b09d0c33dd8ca3c0c7847b3f022732bf2c65d8fe
Tim Zimmermann <tim@linux4.de>
2023-10-06 05:32:56 +0200

    syscall: Fake uname to 5.4.294
    Signed-off-by: agni-prjkt <agnikernel0@gmail.com>
    Signed-off-by: Mr-Morat <mtarom124@gmail.com>

--- kernel/sys.c
+++ kernel/sys.c
@@ -1190,9 +1190,9 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 	memcpy(&tmp, utsname(), sizeof(tmp));
 	if (!strncmp(current->comm, "bpfloader", 9) ||
 	    !strncmp(current->comm, "netd", 4)) {
-		strcpy(tmp.release, "4.19.0");
+		strcpy(tmp.release, "5.4.294");
 		pr_debug("fake uname: %s/%d release=%s\n",
-			 current->comm, current->pid, tmp.release);
+		current->comm, current->pid, tmp.release);
 	}
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
